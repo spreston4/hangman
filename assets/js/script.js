5e2972ec622808d0d956c85acea5e470056e23fe
spreston <sam.preston11@gmail.com>
2021-06-27 10:29:08 -0500

    Validates input & checks for duplicates.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,46 @@
-// Declare array 'wordPool' of words tp pull from
+// Array 'wordPool' to pull word from
 const wordPool = ['abruptly', 'length', 'subway', 'absurd', 'lucky', 'swivel', 'abyss', 'luxury', 'syndrome', 'funny', 'matrix', 'topaz', 'microwave', 'awkward', 'gnarly', 'beekeeper', 'blizzard', 'oxygen', 'bookworm', 'icebox', 'injury', 'puppy', 'wizard', 'vodka', 'wave', 'flapjack', 'knapsack', 'joyful', 'disavow', 'espionage'];
 
+// Array 'letterPool' for input validation
+const letterPool = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+// Empty arrays
 let selectedWord =[];
 let displayArray = [];
+let guessedLetterArray = [];
 
+// HTML elements
 const displayWordEl = $('#display-word');
 const guessLetterEl = $('#guess-letter');
 const submitLetterEl = $('#submit-letter');
+const guessedLettersEl = $('#guessed-letters');
 
 // Function 'getWord' selects a random word from the 'wordPool'
 const getWord = arr => {
+
+    // Get random word from 'wordPool'
     selectedWord = arr[Math.floor(Math.random() * arr.length)].toUpperCase().split('');
-    convertWord(selectedWord);
+
+    // Convert selected word to a hidden display
+    for (let i = 0; i < selectedWord.length; i++) {
+        displayArray[i] = '_'
+    }
+
+    // Send display to page
+    renderWord(displayArray);
+
+    
+    // convertWord(selectedWord);
 }
 
 // Function 'convertWord' takes input from 'getWord' and outputs an array to display to the page
-const convertWord = word => {
+// const convertWord = word => {
 
-    for (let i = 0; i < word.length; i++) {
-        displayArray[i] = '_'
-    }
-    renderWord(displayArray);
-}
+//     for (let i = 0; i < word.length; i++) {
+//         displayArray[i] = '_'
+//     }
+//     renderWord(displayArray);
+// }
 
 // Function 'renderWord' will diplay selected converted word to the page
 const renderWord = arr => {
@@ -33,17 +52,38 @@ const renderWord = arr => {
 // Function 'guessLetter' takes a letter input from the form, searches word array
 const guessLetter = event => {
 
+    // Stop page reload
     event.preventDefault();
+
+    // Get and reset userinput
     let letter = guessLetterEl.val().toUpperCase();
     guessLetterEl.val('');
 
+    // Check for valid character input
+    if (letterPool.includes(letter) === false) {
+        console.log('Please select a valid character(A-Z)')
+        return;
+    }
+
+    // Check for duplicates
+    if (guessedLetterArray.includes(letter) === true) {
+        console.log('Duplicate Detected');
+        return;
+    }
+
+    // Check if correct guess
     for (let i = 0; i < selectedWord.length; i++) {
         if (letter === selectedWord[i]) {
             displayArray[i] = letter;
         }
     }
 
+    // Render to page
     renderWord(displayArray);
+    guessedLetterArray.push(letter);
+    guessedLettersEl.html(`
+        <p>${guessedLetterArray.join(' ')}</p>
+    `);
 }
 
 
