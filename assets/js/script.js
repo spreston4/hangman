9c6b0486a53e88a76241149a5d6672936f0b8465
spreston <sam.preston11@gmail.com>
2021-06-27 08:46:33 -0500

    Displays string to page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,37 @@
 // Declare array 'wordPool' of words tp pull from
 const wordPool = ['abruptly', 'length', 'subway', 'absurd', 'lucky', 'swivel', 'abyss', 'luxury', 'syndrome', 'funny', 'matrix', 'topaz', 'microwave', 'awkward', 'gnarly', 'beekeeper', 'blizzard', 'oxygen', 'bookworm', 'icebox', 'injury', 'puppy', 'wizard', 'vodka', 'wave', 'flapjack', 'knapsack', 'joyful', 'disavow', 'espionage'];
 
+let displayArray = [];
+
+const displayWordEl = $('#display-word');
+
 // Function 'getWord' selects a random word from the 'wordPool'
 const getWord = arr => {
-    let word = arr[Math.floor(Math.random() * arr.length)];
-    console.log(word);
+    let selectedWord = arr[Math.floor(Math.random() * arr.length)].toUpperCase().split('');
+    convertWord(selectedWord);
+
+    console.log('Selected word:');
+    console.log(selectedWord);
+}
+
+// Function 'convertWord' takes input from 'getWord' and outputs an array to display to the page
+const convertWord = word => {
+
+    for (let i = 0; i < word.length; i++) {
+        displayArray[i] = '_'
+    }
+    renderWord(displayArray);
+
+    console.log('Display array: ');
+    console.log(displayArray);
+}
+
+// Function 'renderWord' will diplay selected converted word to the page
+const renderWord = arr => {
+    displayWordEl.html(`
+        <p>${arr.join(' ')}</p>
+    `)
+
 }
 
 
